4b9e7dcdde7ebdf9f29044ba42415ee18ae37b48
Nau Nau <nauanycc@outlook.com.br>
2021-04-27 21:15:57 -0300

    chore: add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 
 const App: React.FC = () => {
-    return (
-        <div className="App">
-            <h1>Hey! You there?</h1>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>Hey! You there?</h1>
+    </div>
+  );
 };
 
 export default App;
