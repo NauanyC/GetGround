693e441331438f281a62489ea4cd831d4f5d116f
Nau Nau <nauanycc@outlook.com.br>
2021-04-28 13:50:06 -0300

    chore: add Book interface

---
+++ src/interfaces/Book.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable camelcase */
+export interface Book {
+  id: number;
+  book_author: string[];
+  book_title: string;
+  book_publication_year: number;
+  book_publication_country: string;
+  book_publication_city: string;
+  book_pages: number;
+}
