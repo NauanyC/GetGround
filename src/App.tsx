6ed39256649fe8a1da7f322a3c4a5bbc6297768e
Nau Nau <nauanycc@outlook.com.br>
2021-04-29 14:07:27 -0300

    feat: add global lightTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import * as React from "react";
 import { BrowserRouter } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 
 import Routes from "./routes";
+import { themeLight } from "./utils/theme";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <BrowserRouter>
-        <CssBaseline />
-        <Routes />
+        <MuiThemeProvider theme={themeLight}>
+          <CssBaseline />
+          <Routes />
+        </MuiThemeProvider>
       </BrowserRouter>
     </div>
   );
---
+++ src/utils/theme.ts
@@ -0,0 +1,15 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export const themeLight = createMuiTheme({
+  palette: {
+    background: {
+      default: "#fff",
+    },
+    primary: {
+      main: "#4d3b15",
+    },
+    secondary: {
+      main: "#5f7778",
+    },
+  },
+});
